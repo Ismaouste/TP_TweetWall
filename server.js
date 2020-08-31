9f3a7b5211b5b802d34267485621e1ea0db33520
Ismael Rodmacq <ismael.rodmacq@gmail.com>
2020-08-31 12:11:37 +0200

    Pushed scripts further with socket config in the client

--- server.js
+++ server.js
@@ -8,28 +8,37 @@ const mongoose = require('mongoose');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-
 // connect  DB
 mongoose.connect('mongodb://localhost:27017/tweetwall', {useNewUrlParser: true, useUnifiedTopology: true});
-
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    // we're connected!
+db.once('open', function () {
     console.log('db connected on port 27017');
 });
 
 // CREATE ROUTES
 app.get('/', function (req, res) {
-    res.sendFile( __dirname +  "/client/static/index.html" );
+    res.sendFile(__dirname + "/client/static/index.html");
 });
 app.get('/tweets', function (req, res) {
-    res.sendFile( __dirname +  "/client/static/tweets.html" );
+    res.sendFile(__dirname + "/client/static/tweets.html");
 });
 
 // START server
-
 http.listen(3011, function () {
     console.log('Example app listening on port 3011!')
 })
+
+//Using Twit to import twitter data
+// io.on('connection', function(socket) {
+//
+//     T.get(
+// )});
+
+var T = new Twit({
+    consumer_key:         '1cp0mey96pOKFScLqGRYRLUFy',
+    consumer_secret:      'cKyGaVWmZdPhYeY6C2jooeNsSF3mPp8VCY6p5gxV6ioh1d4zxn',
+    access_token:         '14107059-RJdiElrn8F88TGD4VnKoyInlR7NENZuYSrUcwei3q%3DXiAiOraDfeEgsoUcyjFRLug2MGs20Hd3TBk9PysN39QkDCmaPo',
+    access_token_secret:  'lrlY2bZ3ch15K9lbjhOxmplBp0jdckDK7kUt0hLfHH7x2',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
